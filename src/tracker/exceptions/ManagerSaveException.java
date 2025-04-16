3bbfedfe96891c79c073c019a3ffd7405c3c221f
Alexander Markov <pwney@yandex.ru>
2025-04-16 13:07:49 +0300

    Добавлен недостающий пробел в ManagerSaveException для прохождения теста CheckStyle

--- src/tracker/exceptions/ManagerSaveException.java
+++ src/tracker/exceptions/ManagerSaveException.java
@@ -1,6 +1,6 @@
 package tracker.exceptions;
 
-public class ManagerSaveException extends RuntimeException{
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
     }
